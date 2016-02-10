082891917c389a537528e01538f9731168e314e2
Benjamin Kleiner <bizzl@users.sourceforge.net>
2016-02-10 17:43:26 +0100

    Added socket

--- index.js
+++ index.js
@@ -2,6 +2,53 @@ const self = require('sdk/self');
 const contextMenu = require('sdk/context-menu');
 const ui = require('sdk/ui');
 const tabs = require('sdk/tabs');
+const pageWorker = require('sdk/page-worker');
+const notifications = require('sdk/notifications');
+const timers = require('sdk/timers');
+
+var socketUrl = 'ws://localhost:8888/';
+
+var socket = pageWorker.Page({
+    contentURL: self.data.url('socket.html')
+});
+
+socket.silent = false;
+socket.available = false;
+
+function socketConnect() {
+    socket.port.emit('open', socketUrl);
+}
+
+socket.port.on('error', console.log);
+
+socket.port.on('message', function(message) {
+    message = JSON.parse(message);
+    notifications.notify({
+        title: message.title,
+        text: message.text
+    });
+});
+
+socket.port.on('closed', function() {
+    if (!socket.silent) {
+        notifications.notify({
+            title: 'Download App disconnected'
+        });
+    }
+    socket.available = false;
+    socket.silent = true;
+    timers.setTimeout(socketConnect, 5000);
+});
+
+socket.port.on('opened', function() {
+    socket.available = true;
+    socket.silent = false;
+    notifications.notify({
+        title: 'Download App connected'
+    });
+});
+
+socketConnect();
 
 function downloadUrl(data) {
     if (data.substr(0, 1) == '{') {
@@ -12,8 +59,10 @@ function downloadUrl(data) {
             referer: data
         };
     }
-    console.log(data);
+    socket.port.emit('send', JSON.stringify(data));
 }
+
+
 
 function DownloadImage() {
     self.on("click", function(node) {
